e67a8bff0e8bd24f117258b9495250b1e0f560c2
Levy <golvwei@gmail.com>
2024-10-21 17:28:35 +0800

    feat: add theme toggle

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,12 +4,12 @@ import { Heroes } from './_components/heroes';
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col dark:bg-dark">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
-        <Footer />
       </div>
+      <Footer />
     </div>
   );
 };
